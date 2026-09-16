46cddd05d35566994ccb0373a380238132295a38
Het Kansara <hetkansara77@gmail.com>
2026-09-17 04:07:13 +0530

    Implemented GET and POST Endpoints for Student API with serializer validation

--- api/views.py
+++ api/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+#from django.shortcuts import render
+#from django.http import JsonResponse
+from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET', 'POST'])
 def studentsView(request):
-    students = [
-    {'id': 1, 'name': 'Raj', 'class': 'Python/Django Developer'},
-    {'id': 2, 'name': 'Het', 'class': 'Python/Django Developer'}
-    ]
-    return JsonResponse(students, safe=False)
+    if request.method == 'GET':
+        student = Student.objects.all()
+        serializer = StudentSerializer(student, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
