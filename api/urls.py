27059b4eb175ff29da324f5f76c9006307c526e0
Het Kansara <hetkansara77@gmail.com>
2026-09-18 04:26:19 +0530

    Created employees app and implemented class based views using APIView, Created EmployeeSerializer.

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #Function-Based URL
     path('students/', views.studentsView),
+    path('students/<int:pk>/', views.studentDetailView),
+
+    #Class-Based URL
+    path('employees/', views.EmployeesView.as_view()),
+    path('employees/<int:pk>/', views.EmployeesDetailView.as_view()),
 ]
